c598a5fcafa800ec2a9c59beda5c91e0a1c5131a
hasrul1056-gomo <hasrul1056@gmail.com>
2026-04-24 12:03:06 +0800

    allow vercel preview URLs via regex

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://erph-frontend.vercel.app'
+    'https://erph-frontend.vercel.app',
+    /^https:\/\/erph-frontend-.*\.vercel\.app$/
   ],
   credentials: true
 }));
